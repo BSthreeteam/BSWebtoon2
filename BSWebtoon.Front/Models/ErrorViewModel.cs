52fa0b0a6c43496020526fa9a79112cc4cd2701f
Kei <zxc045360@gmail.com>
2022-06-20 23:40:38 +0800

    加入專案檔案。

---
+++ BSWebtoon.Front/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BSWebtoon.Front.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
