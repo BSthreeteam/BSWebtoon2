f0e3fa44b67e250adbe4a69ee237b834137de022
徐秉詰 <tuching411086@gmail.com>
2022-07-07 18:47:16 +0800

    comic_create

--- BSWebtoon.Front/Service/MemberService/MemberService.cs
+++ BSWebtoon.Front/Service/MemberService/MemberService.cs
@@ -32,16 +32,16 @@ namespace BSWebtoon.Front.Service.MemberService
                 new Member {MemberId = 9,AccountName ="歐陽愛珊",NickName ="創作大師",Balance =4500,LoginTypeId =3,IsDarkTheme =true,CreateTime =new DateTime(2022,02,05),Email ="uihio@gmail.com",AccessToken ="Sony Xperia 10 III" },
                 new Member {MemberId = 10,AccountName ="于耀成",NickName ="閃耀",Balance =1000,LoginTypeId =1,IsDarkTheme =true,CreateTime =new DateTime(2022,03,18),Email ="trcil432@gmail.com",AccessToken ="Iphone11"},
 
-                new Member {MemberId = 11,AccountName ="林宗揚",NickName ="宗師",Balance =0,LoginTypeId =2,IsDarkTheme =true,CreateTime =new DateTime(2022,04,11),Email ="tometo871@gmail.com",AccessToken ="Iphone12" },
+                new Member {MemberId = 11,AccountName ="林宗揚",NickName ="一代宗師",Balance =0,LoginTypeId =2,IsDarkTheme =true,CreateTime =new DateTime(2022,04,11),Email ="tometo871@gmail.com",AccessToken ="Iphone12" },
                 new Member {MemberId = 12,AccountName ="林美玉",NickName ="美玉",Balance =300,LoginTypeId =1,IsDarkTheme =true,CreateTime =new DateTime(2022,02,02),Email ="apled23@gmail.com",AccessToken ="vivo Y55 5G"},
-                new Member {MemberId = 13,AccountName ="王閣炎",NickName ="小炎",Balance =0,LoginTypeId =1,IsDarkTheme =true,CreateTime =new DateTime(2021,03,21),Email ="ppoppo00@gmail.com",AccessToken ="OPPO A55" },
+                new Member {MemberId = 13,AccountName ="王閣彥",NickName ="小彥",Balance =0,LoginTypeId =1,IsDarkTheme =true,CreateTime =new DateTime(2021,03,21),Email ="ppoppo00@gmail.com",AccessToken ="OPPO A55" },
                 new Member {MemberId = 14,AccountName ="陳亮瑋",NickName ="阿亮",Balance =1000,LoginTypeId =1,IsDarkTheme =false,CreateTime =new DateTime(2022,05,22),Email ="qrqae02311@gmail.com",AccessToken ="Moto Edge 20 Pro"},
-                new Member {MemberId = 15,AccountName ="吳秉喆",NickName ="小喆",Balance =500,LoginTypeId =3,IsDarkTheme =true,CreateTime =new DateTime(2022,06,11),Email ="illibaa31@gmail.com",AccessToken ="Samsung Galaxy S21" },
-                new Member {MemberId = 16,AccountName ="陳柏翰",NickName ="小柏",Balance =1000,LoginTypeId =2,IsDarkTheme =true,CreateTime =new DateTime(2022,03,20),Email ="qwq11@gmail.com",AccessToken ="Iphone8"},
-                new Member {MemberId = 17,AccountName ="李瑩居",NickName ="鄰居",Balance =200,LoginTypeId =3,IsDarkTheme =true,CreateTime =new DateTime(2022,01,18),Email ="mmyvc893222@gmail.com",AccessToken ="realme C21" },
-                new Member {MemberId = 18,AccountName ="陳于瑄",NickName ="小瑄",Balance =1000,LoginTypeId =2,IsDarkTheme =true,CreateTime =new DateTime(2022,04,10),Email ="uouqq9651@gmail.com",AccessToken ="ASUS ROG Phone 5 Pro"},
-                new Member {MemberId = 19,AccountName ="范偉軒",NickName ="大軒",Balance =2300,LoginTypeId =1,IsDarkTheme =true,CreateTime =new DateTime(2022,03,21),Email ="zabcvv31564@gmail.com",AccessToken ="ASUS zenfone 7" },
-                new Member {MemberId = 20,AccountName ="劉秉成",NickName ="supermen",Balance =3000,LoginTypeId =1,IsDarkTheme =false,CreateTime =new DateTime(2022,04,01),Email ="chulove9@gmail.com",AccessToken ="Iphone13"},            };
+                new Member {MemberId = 15,AccountName ="吳秉翰",NickName ="小翰",Balance =500,LoginTypeId =3,IsDarkTheme =true,CreateTime =new DateTime(2022,06,11),Email ="illibaa31@gmail.com",AccessToken ="Samsung Galaxy S21" },
+                new Member {MemberId = 16,AccountName ="陳建宏",NickName ="五菱宏光",Balance =1000,LoginTypeId =2,IsDarkTheme =true,CreateTime =new DateTime(2022,03,20),Email ="qwq11@gmail.com",AccessToken ="Iphone8"},
+                new Member {MemberId = 17,AccountName ="李文樂",NickName ="樂樂",Balance =200,LoginTypeId =3,IsDarkTheme =true,CreateTime =new DateTime(2022,01,18),Email ="mmyvc893222@gmail.com",AccessToken ="realme C21" },
+                new Member {MemberId = 18,AccountName ="陳欣妤",NickName ="小妤",Balance =1000,LoginTypeId =2,IsDarkTheme =true,CreateTime =new DateTime(2022,04,10),Email ="uouqq9651@gmail.com",AccessToken ="ASUS ROG Phone 5 Pro"},
+                new Member {MemberId = 19,AccountName ="范禹彤",NickName ="大禹",Balance =2300,LoginTypeId =1,IsDarkTheme =true,CreateTime =new DateTime(2022,03,21),Email ="zabcvv31564@gmail.com",AccessToken ="ASUS zenfone 7" },
+                new Member {MemberId = 20,AccountName ="劉子晴",NickName ="晴天霹靂",Balance =3000,LoginTypeId =1,IsDarkTheme =false,CreateTime =new DateTime(2022,04,01),Email ="chulove9@gmail.com",AccessToken ="Iphone13"},            };
             foreach (Member member in memberList)
             {
                 _repository.Create(member);
