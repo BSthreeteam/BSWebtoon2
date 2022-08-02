cd3548ba60c36711a80d3ce0249717349cd4b92a
林盈 <B10703007@hotmail.com>
2022-08-03 00:13:32 +0800

    合併小杰

---
+++ BSWebtoon.Front/Models/DTO/Favorite/FavoriteDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace BSWebtoon.Front.Models.DTO.Favorite
+{
+    public partial class FavoriteDTO
+    {
+        public int ComicId { get; set; }
+        public int FavoriteId { get; set; }
+        public int MemberId { get; set; }
+        public string ComicChineseName { get; set; }
+        public string ComicEnglishName { get; set; }
+        public string ComicNameImage { get; set; }
+        public string ComicFigure { get; set; }
+        public string BgColor { get; set; }
+        public string ComicWeekFigure { get; set; }
+    }
+}
