8f168ee3ed5c529911a8c8564188253d8c3061a6
林盈 <B10703007@hotmail.com>
2022-08-05 23:21:21 +0800

    合併均 修改後台及新增後台管理功能

---
+++ BSWebtoon.Admin/Models/DTO/MemberDTO.cs
@@ -0,0 +1,11 @@
+﻿using BSWebtoon.Model.Models;
+using System;
+
+namespace BSWebtoon.Admin.Models.DTO
+{
+    public class MemberDTO:Member
+    {
+        
+        public string LoginTypeName { get; set; }
+    }
+}
