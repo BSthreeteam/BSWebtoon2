54bfcc14d83861b12340b8d19f132ab1b1d13487
林盈 <B10703007@hotmail.com>
2022-08-05 17:10:18 +0800

    修改後台

---
+++ BSWebtoon.Admin/Controllers/WebApi/MemberApiController.cs
@@ -0,0 +1,31 @@
+﻿using BSWebtoon.Admin.IDapperRepository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace BSWebtoon.Admin.Controllers.WebApi
+{
+    [Route("/api/[controller]/[action]")]
+    [ApiController]
+    public class MemberApiController : Controller
+    {
+        protected readonly IDapperMemberRepository _dapperMemberRepository;
+        public MemberApiController (IDapperMemberRepository dapperMemberRepository)
+        {
+            _dapperMemberRepository = dapperMemberRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            try
+            {
+            var result = _dapperMemberRepository.SelectAll();
+            return Ok(result);
+            }
+            catch(Exception ex)
+            {
+                return Ok($"訊息錯誤{ex}");
+            }
+        }
+    }
+}
