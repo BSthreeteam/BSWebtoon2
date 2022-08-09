67fcbe45965fb0953f9b686ed96057335db4b23d
Kei <zxc045360@gmail.com>
2022-08-09 23:09:19 +0800

    後台 排程 活動

---
+++ BSWebtoon.Admin/IDapperRepository/IDapperActivityRepository.cs
@@ -0,0 +1,49 @@
+﻿using BSWebtoon.Admin.Repository;
+using BSWebtoon.Model.Models;
+using Dapper;
+using System.Collections.Generic;
+using System.Data;
+
+namespace BSWebtoon.Admin.IDapperRepository
+{
+    public interface IDapperActivityRepository : IDapperGenericRepository<Activity>
+    {
+
+    }
+    public class DapperActivityRepository :
+        DapperBaseRepository, //繼承這個基底類別
+        IDapperActivityRepository
+    {
+        public DapperActivityRepository(IDbConnection conn) : base(conn)
+        { }
+
+        public int Create(Activity entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int Delete(Activity entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Activity> SelectAll()
+        {
+            return _conn.Query<Activity>(@"Select * from Activity");
+        }
+
+        public Activity SelectById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+
+        public int Update(Activity entity)
+        {
+            return _conn.Execute(@"UPDATE Activity
+                                    SET IsDelete = @IsDelete
+                                    WHERE ActivityId = @ActivityId"
+                                ,entity);
+        }
+    }
+}
---
+++ BSWebtoon.Admin/Service/JobService/CountDownCoupon.cs
@@ -0,0 +1,32 @@
+﻿using BSWebtoon.Admin.IDapperRepository;
+using BSWebtoon.Model.Models;
+using Coravel.Invocable;
+using System;
+using System.Threading.Tasks;
+
+namespace BSWebtoon.Admin.Service.JobService
+{
+    public class CountDownCoupon : IInvocable
+    {
+        private readonly IDapperCouponUseRecordRepository _dapperCouponUseRecordRepository;
+        private readonly IDapperCouponRepository _dapperCouponRepository;
+
+        public CountDownCoupon(IDapperCouponUseRecordRepository dapperCouponUseRecordRepository, IDapperCouponRepository dapperCouponRepository)
+        {
+            _dapperCouponUseRecordRepository = dapperCouponUseRecordRepository;
+            _dapperCouponRepository = dapperCouponRepository;
+        }
+
+        public async Task Invoke()
+        {
+            // couponRecord 篩選 endTime 
+            // coupon 用couponId找couponType 篩選 倒數券
+
+            var coupons = _dapperCouponRepository.SelectAll();
+            var coupoonUseRecord = _dapperCouponUseRecordRepository.SelectAll();
+
+            Console.WriteLine("CountDownCoupon");
+            await Task.CompletedTask;
+        }
+    }
+}
