961a94b5437c6cf3d2fe37b019d193ace564b464
liang <liang10713@gmail.com>
2022-08-10 21:41:26 +0800

    製作後台

---
+++ BSWebtoon.Front/Controllers/UploadComicController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BSWebtoon.Front.Controllers
+{
+    public class UploadComicController : Controller
+    {
+        //[HttpGet("~/[controller]/[action]/{productId}")]
+        public IActionResult UploadComic()
+        {
+            return View();
+        }
+    }
+}
