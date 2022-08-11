7ed22d2a06ba6067ad9a9d075f9900836156de04
Hana <railgun101014@gmail.com>
2022-08-11 13:30:23 +0800

    增加歷史觀看紀錄

--- BSWebtoon.Front/Service/FavoriteService/FavoriteService.cs
+++ BSWebtoon.Front/Service/FavoriteService/FavoriteService.cs
@@ -1,5 +1,4 @@
 ﻿using BSWebtoon.Front.Models.DTO.FavoriteDTO;
-using BSWebtoon.Front.Models.DTO.WorkPage;
 using BSWebtoon.Model.Models;
 using BSWebtoon.Model.Repository;
 using System.Collections.Generic;
@@ -80,55 +79,15 @@ namespace BSWebtoon.Front.Service.FavoriteService
             return result;
         }
 
-
-        //public List<FavoriteDTO> ReadFavorite(int id)
-        //{
-        //    var favoritList = _repository.GetAll<Favorite>().Where(f => f.MemberId == id);
-        //    //var MemberIdList = _repository.GetAll<Member>().OrderBy(e => e.MemberId);
-        //    var ComicIdList = _repository.GetAll<Comic>();
-        //    //var favoritList = _repository.GetAll<Favorite>().Where(c => c.FavoriteId == FavoriteId).First();
-        //    var result = new List<FavoriteDTO>();
-
-        //    List<FavoriteDTO> FavoritList = favoritList.Select(f => new FavoriteDTO
-        //    {
-        //        FavoriteId = f.FavoriteId,
-        //        MemberId = f.MemberId,
-        //        ComicId = f.ComicId,
-        //        ComicChineseName = _repository.GetAll<Comic>().SingleOrDefault(c => c.ComicId == f.ComicId).ComicChineseName,
-        //        ComicEnglishName = _repository.GetAll<Comic>().SingleOrDefault(c => c.ComicId == f.ComicId).ComicEnglishName,
-        //        ComicNameImage = _repository.GetAll<Comic>().SingleOrDefault(c => c.ComicId == f.ComicId).ComicNameImage,
-        //        ComicFigure = _repository.GetAll<Comic>().SingleOrDefault(c => c.ComicId == f.ComicId).ComicFigure,
-        //        BgColor = _repository.GetAll<Comic>().SingleOrDefault(c => c.ComicId == f.ComicId).BgColor,
-        //        ComicWeekFigure = _repository.GetAll<Comic>().SingleOrDefault(c => c.ComicId == f.ComicId).ComicWeekFigure,
-        //    }).ToList();
-        //    result.AddRange(FavoritList);
-
-        //    return result;
-        //}
-
-
-        //public IEnumerable<FavoriteDTO> GetFavorite()
-        //{
-        //    return from member in _repository.GetAll<Member>()
-        //           join comic in _repository.GetAll<Comic>()
-        //           on member.MemberId equals comic.ComicId
-        //           where member.MemberId == 1
-        //           select new FavoriteDTO
-        //           {
-        //               ComicFigure = comic.ComicFigure,
-        //               ComicNameImage = comic.ComicNameImage,
-        //               BgColor = comic.BgColor
-        //           };
-        //}
-
-
-
         public void FavoriteDelete(int id)
         {
             var DelFavorite = _repository.GetAll<Favorite>().Where(f => f.MemberId == 3).FirstOrDefault();
             _repository.Delete(DelFavorite);
             _repository.SaveChange();
         }
+
+
+
         public void RemoveFavoriteRecord(RemoveFavoriteInputDTO input)
         {
             var DelFavorite = _repository.GetAll<Favorite>()
@@ -144,27 +103,5 @@ namespace BSWebtoon.Front.Service.FavoriteService
             _repository.SaveChange();
 
         }
-        public void FavoriteDataCreateOrDelete(FavoriteDataDTO favoriteData)
-        {
-            var sameData = _repository.GetAll<Favorite>()
-                .Where(f => f.MemberId == favoriteData.MemberId && f.ComicId == favoriteData.ComicId).FirstOrDefault();
-
-            if (favoriteData.IsLike == true && sameData == null)
-            {
-                var favorite = new Favorite
-                {
-                    ComicId = favoriteData.ComicId,
-                    MemberId = favoriteData.MemberId,
-                };
-                _repository.Create(favorite);
-            }
-            else if (favoriteData.IsLike == false && sameData != null)
-            {
-                _repository.Delete(sameData);
-            }
-            _repository.SaveChange();
-        }
-
-
     }
 }
